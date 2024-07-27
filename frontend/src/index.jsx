3c207c74c6382651e518e95c40a0eb96b45aa9e2
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-27 14:13:06 -0400

    Implement React Router

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 
 import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />}></Route>
+    </Route>,
+  ),
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />,
+);
