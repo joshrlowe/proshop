ee14a1a0bac573d54796b4b9b1090a1435f6f687
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-28 11:27:56 -0400

    Add Product Details Page

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -11,11 +11,17 @@ import "./assets/styles/index.css";
 
 import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
+      <Route
+        index={true}
+        path="/product/:id"
+        element={<ProductScreen />}
+      ></Route>
     </Route>,
   ),
 );
