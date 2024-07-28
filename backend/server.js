238ac92fb1c94c53953da830d41177646358e7a2
Josh Lowe <joshlowe.cs@gmail.com>
2024-07-28 12:26:09 -0400

    Configure environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
 import products from "./data/products.js";
-const port = 5001;
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
